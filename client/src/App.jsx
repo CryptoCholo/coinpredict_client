6e88f6a7c62f6c54209dcb13c776bf26660d5c4e
cryptojunkie <mrcholo21@icloud.com>
2023-04-30 19:49:14 +0100

    updated links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,9 +12,9 @@ export default function App() {
           <Route path="https://coin-forecast.onrender.com/" element={<Nav/>}>
             <Route index element={<Home/>}/>
           <Route path="https://coin-forecast.onrender.com/btc" element={<CryptoChart dataUrl="https://coinpredict.onrender.com/btc" crypto="Bitcoin"/>} />
-          <Route path="https://coin-forecast.onrender.com/bnb" element={<CryptoChart dataUrl="https://coinpredict.onrender.com//bnb" crypto="Binance coin"/>} />
-          <Route path="https://coin-forecast.onrender.com/eth" element={<CryptoChart dataUrl="https://coinpredict.onrender.com//eth" crypto="Ethereum"/>}/>
-          <Route path="https://coin-forecast.onrender.com/sol" element={<CryptoChart dataUrl="https://coinpredict.onrender.com//sol" crypto="Solana"/>} />
+          <Route path="https://coin-forecast.onrender.com/bnb" element={<CryptoChart dataUrl="https://coinpredict.onrender.com/bnb" crypto="Binance coin"/>} />
+          <Route path="https://coin-forecast.onrender.com/eth" element={<CryptoChart dataUrl="https://coinpredict.onrender.com/eth" crypto="Ethereum"/>}/>
+          <Route path="https://coin-forecast.onrender.com/sol" element={<CryptoChart dataUrl="https://coinpredict.onrender.com/sol" crypto="Solana"/>} />
           </Route>
         </Routes>
       </BrowserRouter> 
